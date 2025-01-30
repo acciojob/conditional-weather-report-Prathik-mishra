61124c23bf18a88360b4a5c46767a2ce4c47048a
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2025-01-30 19:29:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,14 @@
-
 import React, { useEffect, useState } from "react";
-import './../styles/App.css';
 import WeatherDisplay from "./WeatherDisplay";
-
-
 const def = {temperature : 0, conditions : "Cool"}
-
 const App = () => {
-
-  const[info, setInfo] = useState(def)
-
-
-  useEffect(() => {
+const[info, setInfo] = useState(def)
+useEffect(() => {
     setTimeout(() => {
       setInfo({ temperature: 25, conditions: "Sunny" })
     }, 1000)
   })
-
-  return (
+return (
     <div>
       <WeatherDisplay info={info} />
     </div>
